21a0ff2e5ff76517b004dd74ea39f0da18c0a3ec
Ruchika Saboo <ruchikas@cybage.com>
2016-04-13 12:24:57 +0530

    Refactoring of leave management files
    Signed-off-by: Ruchika Saboo <ruchikas@cybage.com>

--- app/src/leaveManagement/client/js/services/service.js
+++ app/src/leaveManagement/client/js/services/service.js
@@ -3,7 +3,6 @@ angular.module('myApp.services', [])
 .service('EmployeeService', function () {
   this.empCode;
 
-
     this.getEmployee = function() {
       return this.empCode;
 
@@ -12,4 +11,60 @@ angular.module('myApp.services', [])
       console.log("code" + code);
       this.empCode = code;
     }
+})
+
+.factory('leaveMgtFactory', function($http, $q) {
+    return {
+        getEmployee: function() {
+
+	        var deferred = $q.defer();
+	        $http.get("leave-management/api/employee/")
+	        .success(function(data) {
+		        deferred.resolve(data);
+		    })
+            .error(function(error) {
+                deferred.reject(error);
+            });
+            return deferred.promise
+        },
+
+		getFloatingLeaves: function() {
+
+			var deferred = $q.defer();
+			$http.get("/data/ft.json")
+			.success(function(data) {
+		        deferred.resolve(data);
+		    })
+		    .error(function(error) {
+		        deferred.reject(error);
+		    });
+			return deferred.promise
+		},
+
+		getEmployeeLeaves: function(empcode) {
+
+			var deferred = $q.defer();
+			$http.get('leave-management/api/leaveList/' + empcode)
+			.success(function(data) {
+		        deferred.resolve(data);
+		    })
+		    .error(function(error) {
+		        deferred.reject(error);
+		    });
+			return deferred.promise
+		},
+
+		applyLeave: function(res) {
+
+			var deferred = $q.defer();
+	        $http.post('leave-management/api/leave', res)
+	        .success(function(res) {
+		        deferred.resolve(res);
+	        })
+	        .error(function(error) {
+		        deferred.reject(error);
+	        });
+			return deferred.promise
+		}
+    };
 });
